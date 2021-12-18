e9f086d1d0cda2c2219b68b619e927acfcbf087d
Juan Menezes <juan.menezes@outlook.com>
2021-12-18 14:21:43 -0300

    implementacao da conexao entre o app e o collab

--- lib/Telas/teste_request.dart
+++ lib/Telas/teste_request.dart
@@ -4,31 +4,34 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<Album> createAlbum(String title) async {
-  final response = await http.post(Uri.http(
-      "5a2c-34-105-45-42.ngrok.io", "/classify_student", {"student": "juan"}));
+Future<AlgoritmoCollab> createAlgoritmoCollab(String valorTotal, String quadra,
+    String biblioteca, String refeitorio, String internet) async {
+  final response = await http
+      .post(Uri.http("12d7-34-74-10-207.ngrok.io", "/classifica_escola", {
+    "valorTotalParam": valorTotal,
+    "quadraParam": quadra,
+    "bibliotecaParam": biblioteca,
+    "refeitorioParam": refeitorio,
+    "internetParam": internet,
+  }));
 
-  if (response.statusCode == 201 || response.statusCode == 404) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
+  if (response.statusCode == 201 ||
+      response.statusCode == 404 ||
+      response.statusCode == 200) {
+    return AlgoritmoCollab.fromJson(jsonDecode(response.body));
   } else {
-    // If the server did not return a 201 CREATED response,
-    // then throw an exception.
-    throw Exception('Failed to create album.');
+    throw Exception('Falha ao se comunicar com o servidor');
   }
 }
 
-class Album {
-  final int id;
+class AlgoritmoCollab {
   final String title;
 
-  Album({required this.id, required this.title});
+  AlgoritmoCollab({required this.title});
 
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      id: json['id'],
-      title: json['title'],
+  factory AlgoritmoCollab.fromJson(Map<String, dynamic> json) {
+    return AlgoritmoCollab(
+      title: json['escolaModelo_name'],
     );
   }
 }
@@ -43,24 +46,31 @@ class TesteRequest extends StatefulWidget {
 }
 
 class _TesteRequestState extends State<TesteRequest> {
-  final TextEditingController _controller = TextEditingController();
-  Future<Album>? _futureAlbum;
+  final TextEditingController valorTotalController = TextEditingController();
+  final TextEditingController quadraController = TextEditingController();
+  final TextEditingController bibliotecaController = TextEditingController();
+  final TextEditingController refeitorioController = TextEditingController();
+  final TextEditingController internetController = TextEditingController();
+
+  Future<AlgoritmoCollab>? _futureAlgoritmoCollab;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Create Data Example',
+      title: 'Classificador de aprovação',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Create Data Example'),
+          title: const Text('Resultado Classificador de Aprovação'),
         ),
         body: Container(
           alignment: Alignment.center,
           padding: const EdgeInsets.all(8.0),
-          child: (_futureAlbum == null) ? buildColumn() : buildFutureBuilder(),
+          child: (_futureAlgoritmoCollab == null)
+              ? buildColumn()
+              : buildFutureBuilder(),
         ),
       ),
     );
@@ -70,28 +80,57 @@ class _TesteRequestState extends State<TesteRequest> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        TextField(
-          controller: _controller,
-          decoration: const InputDecoration(hintText: 'Enter Title'),
+        TextFormField(
+          controller: valorTotalController,
+          decoration:
+              const InputDecoration(hintText: 'Digite o valor total previsto'),
+        ),
+        TextFormField(
+          controller: quadraController,
+          decoration: const InputDecoration(
+              hintText: 'Digite se tem QUADRA DE ESPORTES'),
+        ),
+        TextFormField(
+          controller: bibliotecaController,
+          decoration: const InputDecoration(
+              hintText: 'Digite se tem BIBLIOTECA/SALA DE LEITURA'),
+        ),
+        TextFormField(
+          controller: refeitorioController,
+          decoration:
+              const InputDecoration(hintText: 'Digite se tem REFEITORIO'),
+        ),
+        TextFormField(
+          controller: internetController,
+          decoration:
+              const InputDecoration(hintText: 'Digite se tem IN_INTERNET'),
         ),
         ElevatedButton(
           onPressed: () {
             setState(() {
-              _futureAlbum = createAlbum(_controller.text);
+              _futureAlgoritmoCollab = createAlgoritmoCollab(
+                  valorTotalController.text,
+                  quadraController.text,
+                  bibliotecaController.text,
+                  refeitorioController.text,
+                  internetController.text);
             });
           },
-          child: const Text('Create Data'),
+          child: const Text('Verificar resultado'),
         ),
       ],
     );
   }
 
-  FutureBuilder<Album> buildFutureBuilder() {
-    return FutureBuilder<Album>(
-      future: _futureAlbum,
+  FutureBuilder<AlgoritmoCollab> buildFutureBuilder() {
+    return FutureBuilder<AlgoritmoCollab>(
+      future: _futureAlgoritmoCollab,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return Text(snapshot.data!.title);
+          //AQUI É O RETORNO DE QUANDO ELE EXIBE O TEXTO QUE O SERVIDOR TROUXE COMO RESPOSTA
+          return Column(
+            children: [Text(snapshot.data!.title)],
+          );
         } else if (snapshot.hasError) {
           return Text('${snapshot.error}');
         }
